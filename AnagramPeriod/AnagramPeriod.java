516ee2de1a174c38f4a1a2efd1697cfc9722c292
Zakaria Benmoulay <zakaria@192.168.1.7>
2024-02-03 15:31:14 -0500

    AnagramPeriod

---
+++ AnagramPeriod/AnagramPeriod.java
@@ -0,0 +1,12 @@
+
+public class AnagramPeriod {
+
+  public static void main(String[] args) {
+    getAnagramPeriod("ababbaab");
+  }
+
+  private static void getAnagramPeriod(String input_str) {
+    int n = input_str.length();
+    System.out.println(n);
+  }
+}
